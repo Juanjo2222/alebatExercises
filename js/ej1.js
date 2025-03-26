b84d2fe0583d2b53911d2ec60509d7f929e8e3a7
Juan Jose <juanjomillanguerra@gmail.com>
2025-03-26 11:24:35 +0100

    ejercicio 1 de js completado

---
+++ js/ej1.js
@@ -0,0 +1,4 @@
+const userName = "admin"; //camelCase
+const usernameInKebabCase = "admin"; //en kebab case sería user-name
+const user_name = "admin"; //snake_Case
+const UserName = "admin"; //PascalCase
