ffe4276659b4115b657a7300c88f5123f59ed56d
Juan Jose <juanjomillanguerra@gmail.com>
2025-03-26 11:55:19 +0100

    primer codigo todo en ingles y segundo codigo completado

--- js/ej4.js
+++ js/ej4.js
@@ -1,16 +1,35 @@
 //PRIMER CODIGO
 
-let usuario = {
-  nombre: "Pedro",
-  apellido: "Sánchez",
-  edad: 45,
+const user1 = {
+  name: "Pedro",
+  last: "Sánchez",
+  age: 45,
+  profession: "Barro man",
+};
+
+function userName({ name, last }) {
+  const fullName = `Me llamo ${name} ${last}`;
+
+  return fullName;
+}
+
+console.log(userName(user1));
+
+//SEGUNDO CODIGO
+
+const user2 = {
+  name: "Pedro",
+  last: "Sánchez",
+  age: 45,
+  salary: 1000,
   profesion: "Barro man",
 };
 
-function nombreUsuario({ nombre, apellido }) {
-  const nombrCompleto = `Me llamo ${nombre} ${apellido}`;
+function userData({ name, last, salary }) {
+  const annualSalary = salary * 12;
+  const fullName = `${name} ${last}`;
 
-  return nombrCompleto;
+  return `Me llamo ${fullName} y cobro ${annualSalary}€ al año`;
 }
 
-console.log(nombreUsuario(usuario));
+console.log(userData(user2));
